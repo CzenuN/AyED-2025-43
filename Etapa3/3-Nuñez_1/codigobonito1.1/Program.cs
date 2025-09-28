7da6768039f75748d245c7db5c2b466b9e9a66b5
Cecilia Nuñez <cecilianunez.sixx@gmail.com>
2025-09-28 15:28:38 -0300

    Add files via upload

---
+++ Etapa3/3-Nuñez_1/codigobonito1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigobonito1._1
+{
+    class Program
+    {
+        static int suma(int Val1, int Val2)
+        {
+            return (Val1 + Val2);
+        }
+        static void Main(string[] args)
+        {
+            int result = suma(4, 6);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ Etapa3/3-nuñez_0/codigobonito1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigobonito1
+{
+    class Program
+    {
+        static string saludo(string nombre)
+        {
+            return "Hola " + nombre ;
+        }
+        
+
+        static void Main(string[] args)
+        {
+
+            string nom = Console.ReadLine();
+            string mensaje = saludo(nom);
+            Console.WriteLine(mensaje);
+            Console.ReadKey();
+        }
+
+    }
+}

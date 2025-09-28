6dc45bce5c5fd642c28dbc1f36e2d2ce1abc8a87
Cecilia Nuñez <cecilianunez.sixx@gmail.com>
2025-09-28 14:16:13 -0300

    Add files via upload

---
+++ Etapa0/0-Nuñez-8/0-Nuñez-8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nuñez_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dividendo: ");
+            int dividendo = int.Parse(Console.ReadLine());
+            Console.Write("Divisor: ");
+            int divisor = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resto: {dividendo % divisor}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa0/0-Nuñez-9/0-Nuñez-9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nuñez_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Radio: ");
+            double radio = double.Parse(Console.ReadLine());
+            double superficie = 4 * Math.PI * radio * radio;
+            double volumen = (4.0 / 3.0) * Math.PI * radio * radio * radio;
+            Console.WriteLine($"Superficie: {superficie}");
+            Console.WriteLine($"Volumen: {volumen}");
+
+            Console.ReadKey();
+        }
+    }
+}
+

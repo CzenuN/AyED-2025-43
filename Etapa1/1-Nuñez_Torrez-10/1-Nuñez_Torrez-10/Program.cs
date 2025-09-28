d4e6931b42b059fb2205aa659b97031f843edfe1
Cecilia Nuñez <cecilianunez.sixx@gmail.com>
2025-09-28 14:36:21 -0300

    Add files via upload

---
+++ Etapa1/1-Nuñez_Torrez-10/1-Nuñez_Torrez-10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nuñez_Torrez_10
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+            Console.WriteLine(
+                (i % 3 == 0 && i % 5 == 0) ? "fizzbuzz" :
+                (i % 3 == 0) ? "fizz" :
+                (i % 5 == 0) ? "buzz" : "" + i
+            );
+
+        Console.ReadKey();
+    }
+}
